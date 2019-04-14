dc9fff40ebc1e4bf49e8e6fc1a7e6bd4e182a9ec
Francis Quenneville <francis.quenneville.2@ens.etsmtl.ca>
2019-04-14 16:26:10 -0400

    Swagger operations bug fixed

--- src/eDoxa.Swagger/Filters/CustomOperationFilter.cs
+++ src/eDoxa.Swagger/Filters/CustomOperationFilter.cs
@@ -17,7 +17,6 @@ using eDoxa.Seedwork.Infrastructure.Constants;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Net.Http.Headers;
 
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -28,19 +27,9 @@ namespace eDoxa.Swagger.Filters
     {
         public void Apply(Operation operation, OperationFilterContext context)
         {
-            operation.Parameters = new List<IParameter>();
-
-            operation.Responses = new Dictionary<string, Response>();
-
-            operation.Security = new List<IDictionary<string, IEnumerable<string>>>();
-
-            operation.Tags = new List<string>();
-
             ApplyGroupNameOperation(operation, context);
 
             ApplySecurityOperation(operation, context);
-
-            ApplyIdempotencyKeyOperation(operation, context);
         }
 
         private static void ApplyGroupNameOperation(Operation operation, OperationFilterContext context)
@@ -55,7 +44,10 @@ namespace eDoxa.Swagger.Filters
             {
                 var groupName = urlSegment[0].ToString().ToUpper() + urlSegment.Substring(1);
 
-                operation.Tags.Add(groupName);
+                operation.Tags = new List<string>
+                {
+                    groupName
+                };
             }
         }
 
@@ -63,44 +55,55 @@ namespace eDoxa.Swagger.Filters
         {
             if (context.ApiDescription.TryGetMethodInfo(out var methodInfo))
             {
-                var authorize = methodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
-                var allowAnonymous = methodInfo.DeclaringType.GetCustomAttributes(true).OfType<AllowAnonymousAttribute>().Any();
-
-                if (authorize && !allowAnonymous)
+                if (methodInfo.DeclaringType != null)
                 {
-                    operation.Responses.Add(
-                        StatusCodes.Status401Unauthorized.ToString(),
-                        new Response
-                        {
-                            Description = HttpStatusCode.Unauthorized.ToString()
-                        }
-                    );
+                    var authorize = methodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+                    var allowAnonymous = methodInfo.DeclaringType.GetCustomAttributes(true).OfType<AllowAnonymousAttribute>().Any();
 
-                    operation.Responses.Add(
-                        StatusCodes.Status403Forbidden.ToString(),
-                        new Response
-                        {
-                            Description = HttpStatusCode.Forbidden.ToString()
-                        }
-                    );
-
-                    operation.Security.Add(
-                        new Dictionary<string, IEnumerable<string>>
-                        {
+                    if (authorize && !allowAnonymous)
+                    {
+                        operation.Responses.Add(
+                            StatusCodes.Status401Unauthorized.ToString(),
+                            new Response
                             {
-                                "oauth2", new[]
+                                Description = HttpStatusCode.Unauthorized.ToString()
+                            }
+                        );
+
+                        operation.Responses.Add(
+                            StatusCodes.Status403Forbidden.ToString(),
+                            new Response
+                            {
+                                Description = HttpStatusCode.Forbidden.ToString()
+                            }
+                        );
+
+                        //operation.Parameters.Add(
+                        //    new NonBodyParameter
+                        //    {
+                        //        Name = HeaderNames.Authorization,
+                        //        In = "header",
+                        //        Required = true,
+                        //        Type = "string"
+                        //    }
+                        //);
+
+                        operation.Security = new List<IDictionary<string, IEnumerable<string>>>
+                        {
+                            new Dictionary<string, IEnumerable<string>>
+                            {
                                 {
-                                    CustomScopes.IdentityApi, CustomScopes.CashierApi, CustomScopes.ChallengesApi, CustomScopes.NotificationsApi
+                                    "oauth2", new[]
+                                    {
+                                        CustomScopes.IdentityApi, CustomScopes.CashierApi, CustomScopes.ChallengesApi, CustomScopes.NotificationsApi
+                                    }
                                 }
                             }
-                        }
-                    );
+                        };
+                    }
                 }
             }
-        }
 
-        private static void ApplyIdempotencyKeyOperation(Operation operation, OperationFilterContext context)
-        {
             if (context.ApiDescription.HttpMethod == HttpMethods.Put ||
                 context.ApiDescription.HttpMethod == HttpMethods.Post ||
                 context.ApiDescription.HttpMethod == HttpMethods.Delete ||
@@ -112,25 +115,6 @@ namespace eDoxa.Swagger.Filters
                         Name = CustomHeaderNames.IdempotencyKey, In = "header", Type = "string", Required = false
                     }
                 );
-            }
-        }
-
-        private static void ApplyAuthorizationOperation(Operation operation, OperationFilterContext context)
-        {
-            if (context.ApiDescription.TryGetMethodInfo(out var methodInfo))
-            {
-                var authorize = methodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
-                var allowAnonymous = methodInfo.DeclaringType.GetCustomAttributes(true).OfType<AllowAnonymousAttribute>().Any();
-
-                if (authorize && !allowAnonymous)
-                {
-                    operation.Parameters.Add(
-                        new NonBodyParameter
-                        {
-                            Name = HeaderNames.Authorization, In = "header", Required = true, Type = "string"
-                        }
-                    );
-                }
             }
         }
     }
