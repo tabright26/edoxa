8fe0f6604ebc8a4d301bdb436e573018953ad603
Francis Quenneville <francis.quenneville.2@ens.etsmtl.ca>
2019-04-22 18:18:27 -0400

    Challenge timeline typed objects

---
+++ src/eDoxa.Challenges.Domain/AggregateModels/ChallengeAggregate/Temp/CreateAt.cs
@@ -0,0 +1,34 @@
+﻿// Filename: CreateAt.cs
+// Date Created: 2019-04-22
+// 
+// ================================================
+// Copyright © 2019, eDoxa. All rights reserved.
+//  
+// This file is subject to the terms and conditions
+// defined in file 'LICENSE.md', which is part of
+// this source code package.
+
+using System;
+
+namespace eDoxa.Challenges.Domain.AggregateModels.ChallengeAggregate.Temp
+{
+    public sealed class CreateAt
+    {
+        private readonly DateTime _value;
+
+        public CreateAt()
+        {
+            _value = DateTime.UtcNow;
+        }
+
+        internal CreateAt(DateTime createAt)
+        {
+            _value = createAt;
+        }
+
+        public static implicit operator DateTime(CreateAt createAt)
+        {
+            return createAt._value;
+        }
+    }
+}
---
+++ src/eDoxa.Challenges.Domain/AggregateModels/ChallengeAggregate/Temp/StartedAt.cs
@@ -0,0 +1,34 @@
+﻿// Filename: StartedAt.cs
+// Date Created: 2019-04-22
+// 
+// ================================================
+// Copyright © 2019, eDoxa. All rights reserved.
+//  
+// This file is subject to the terms and conditions
+// defined in file 'LICENSE.md', which is part of
+// this source code package.
+
+using System;
+
+namespace eDoxa.Challenges.Domain.AggregateModels.ChallengeAggregate.Temp
+{
+    public sealed class StartedAt
+    {
+        private readonly DateTime _value;
+
+        public StartedAt(PublishedAt publishedAt, RegistrationPeriod registrationPeriod)
+        {
+            _value = publishedAt + registrationPeriod;
+        }
+
+        public static implicit operator DateTime(StartedAt startedAt)
+        {
+            return startedAt._value;
+        }
+
+        public static EndedAt operator +(StartedAt startedAt, ExtensionPeriod extensionPeriod)
+        {
+            return new EndedAt(startedAt, extensionPeriod);
+        }
+    }
+}
