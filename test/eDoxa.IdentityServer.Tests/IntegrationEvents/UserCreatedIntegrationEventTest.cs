e9a481200f9e8e09581d72181e182534b89368f0
Francis Quenneville <francis.quenneville.2@ens.etsmtl.ca>
2019-05-13 21:08:36 -0400

    json refactoring

---
+++ test/eDoxa.IdentityServer.Tests/IntegrationEvents/UserCreatedIntegrationEventTest.cs
@@ -0,0 +1,31 @@
+﻿// Filename: UserCreatedIntegrationEventTest.cs
+// Date Created: 2019-05-13
+// 
+// ================================================
+// Copyright © 2019, eDoxa. All rights reserved.
+// 
+// This file is subject to the terms and conditions
+// defined in file 'LICENSE.md', which is part of
+// this source code package.
+
+using System;
+
+using eDoxa.IdentityServer.IntegrationEvents;
+using eDoxa.Testing.MSTest;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace eDoxa.IdentityServer.Tests.IntegrationEvents
+{
+    [TestClass]
+    public sealed class UserCreatedIntegrationEventTest
+    {
+        [TestMethod]
+        public void Constructor_Tests()
+        {
+            ConstructorTests<UserCreatedIntegrationEvent>.For(typeof(Guid), typeof(string), typeof(string), typeof(string))
+                .WithName("UserCreatedIntegrationEvent")
+                .Assert();
+        }
+    }
+}
