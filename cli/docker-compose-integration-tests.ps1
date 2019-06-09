cf428bea0a674c82b9bf29229e3f202531692473
Francis Quenneville <francis.quenneville.2@ens.etsmtl.ca>
2019-06-08 20:29:05 -0400

    docker-compose as powershell scripts

---
+++ cli/docker-compose-integration-tests.ps1
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose-integration-tests.yml -f docker-compose-integration-tests.override.yml up --build --remove-orphans
---
+++ cli/docker-compose-unit-tests.ps1
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose-unit-tests.yml up --build
