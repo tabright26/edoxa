23a235193e7b206ba1db7ef5b451233c539d6949
Francis Quenneville <francis.quenneville.2@ens.etsmtl.ca>
2019-07-23 20:40:04 -0400

    Specification for paralization levels

---
+++ test/eDoxa.FunctionalTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// Filename: AssemblyInfo.cs
+// Date Created: 2019-07-23
+// 
+// ================================================
+// Copyright © 2019, eDoxa. All rights reserved.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: DoNotParallelize]
