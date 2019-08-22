bd84af02cf09ca49bd8d08e4bfaa685df3d3b1d1
Gabriel Roy <gabriel@edoxa.gg>
2019-08-22 16:43:11 -0400

    Cashier Done
    Related Work Items: #633, #634

---
+++ test/eDoxa.Cashier.UnitTests/IntegrationEvents/Handlers/UserTransactionSuccededIntegrationEventHandlerTest.cs
@@ -0,0 +1,46 @@
+﻿// Filename: UserCreatedIntegrationEventHandlerTest.cs
+// Date Created: 2019-06-25
+// 
+// ================================================
+// Copyright © 2019, eDoxa. All rights reserved.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using eDoxa.Cashier.Api.Application.Requests;
+using eDoxa.Cashier.Api.IntegrationEvents;
+using eDoxa.Cashier.Api.IntegrationEvents.Handlers;
+using eDoxa.Cashier.Domain.Repositories;
+
+using MediatR;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+namespace eDoxa.Cashier.UnitTests.IntegrationEvents.Handlers
+{
+    [TestClass]
+    public sealed class UserTransactionSuccededIntegrationEventHandlerTest
+    {
+        [TestMethod]
+        public async Task UserTransactionFailedIntegrationEvent_ShouldBeCompletedTask()
+        {
+            // Arrange
+            var mockRepository = new Mock<ITransactionRepository>();
+
+            mockRepository.Setup(transaction => transaction);
+
+            var handler = new UserTransactionSuccededIntegrationEventHandler(mockRepository.Object);
+
+            var integrationEvent = new UserTransactionSuccededIntegrationEvent(Guid.NewGuid());
+
+            // Act
+            await handler.HandleAsync(integrationEvent);
+
+            // Assert
+            mockRepository.Verify(transaction => transaction);
+        }
+    }
+}
---
+++ test/eDoxa.Cashier.UnitTests/IntegrationEvents/UserCreatedIntegrationEventTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using eDoxa.Cashier.Api.IntegrationEvents;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Newtonsoft.Json;
+
+namespace eDoxa.Cashier.UnitTests.IntegrationEvents
+{
+    [TestClass]
+    public sealed class UserCreatedIntegrationEventTest
+    {
+        [TestMethod]
+        public void UserCreatedIntegrationEventTest_WithNewUserAccount_ShouldBeEquivalentToUserCreationEvent()
+        {
+            //Arrange
+            var userCreationEvent = new UserCreatedIntegrationEventTest();
+
+            var serializedEvent = JsonConvert.SerializeObject(userCreationEvent);
+
+            //Act
+            var deserializedEvent = JsonConvert.DeserializeObject<UserCreatedIntegrationEventTest>(serializedEvent);
+
+            //Assert
+            deserializedEvent.Should().BeEquivalentTo(userCreationEvent);
+        }
+    }
+}
