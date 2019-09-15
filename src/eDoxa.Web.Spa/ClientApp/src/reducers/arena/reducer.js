d9c8070b0263e5399d60e33a36d4cac50e407c82
Francis Quenneville <francis@edoxa.gg>
2019-09-14 21:13:40 -0400

    Redux persit removed

--- src/eDoxa.Web.Spa/ClientApp/src/reducers/arena/reducer.js
+++ src/eDoxa.Web.Spa/ClientApp/src/reducers/arena/reducer.js
@@ -3,19 +3,7 @@ import { combineReducers } from "redux";
 import { reducer as challenges } from "./challenges/reducer";
 import { reducer as games } from "./games/reducer";
 
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage/session";
-
-const persistConfig = {
-  key: "arena",
-  storage,
-  blacklist: ["challenges"]
-};
-
-export const reducer = persistReducer(
-  persistConfig,
-  combineReducers({
-    challenges,
-    games
-  })
-);
+export const reducer = combineReducers({
+  challenges,
+  games
+});
