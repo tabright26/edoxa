eab29f98fe4d029e39d5a1f2e56242b7302a6cf2
Francis Quenneville <francis@edoxa.gg>
2019-10-07 01:35:17 -0400

    Clans database context seeder updated

--- src/eDoxa.Organizations.Clans.Api/Infrastructure/Data/ClansDbContextSeeder.cs
+++ src/eDoxa.Organizations.Clans.Api/Infrastructure/Data/ClansDbContextSeeder.cs
@@ -17,36 +17,34 @@ using Microsoft.Extensions.Logging;
 
 namespace eDoxa.Organizations.Clans.Api.Infrastructure.Data
 {
-    internal sealed class ClansDbContextSeeder : IDbContextSeeder
+    internal sealed class ClansDbContextSeeder : DbContextSeeder
     {
         private readonly ClansDbContext _context;
         private readonly IClanRepository _clanRepository;
-        private readonly ILogger<ClansDbContextSeeder> _logger;
-        private readonly IHostingEnvironment _environment;
 
-        public ClansDbContextSeeder(ILogger<ClansDbContextSeeder> logger, IHostingEnvironment environment, ClansDbContext context, IClanRepository clanRepository)
+        public ClansDbContextSeeder(
+            ClansDbContext context,
+            IClanRepository clanRepository,
+            IHostingEnvironment environment,
+            ILogger<ClansDbContextSeeder> logger
+        ) : base(environment, logger)
         {
-            _logger = logger;
-            _environment = environment;
             _context = context;
             _clanRepository = clanRepository;
         }
 
-        public async Task SeedAsync()
+        protected override async Task SeedDevelopmentAsync()
         {
-            if (_environment.IsDevelopment())
+            if (!_context.Clans.Any())
             {
-                if (!_context.Clans.Any())
+                var clans = FileStorage.Clans;
+
+                foreach (var clan in clans)
                 {
-                    var clans = FileStorage.Clans;
-
-                    foreach (var clan in clans)
-                    {
-                        _clanRepository.Create(clan);
-                    }
-
-                    await _clanRepository.UnitOfWork.CommitAsync();
+                    _clanRepository.Create(clan);
                 }
+
+                await _clanRepository.UnitOfWork.CommitAsync();
             }
         }
     }
