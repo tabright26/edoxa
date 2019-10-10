d6d510ed649313eb22ab08e20a7c838f93eb2e25
Francis Quenneville <francis@edoxa.gg>
2019-10-10 11:33:54 -0400

    Temp tests removed

--- test/eDoxa.Payment.UnitTests/Test.cs
+++ test/eDoxa.Payment.UnitTests/Test.cs
@@ -22,109 +22,109 @@ namespace eDoxa.Payment.UnitTests
         {
         }
 
-        public async Task<Account> TestAccount(string email, PersonCreateOptions individual, AccountBankAccountOptions externalAccount)
-        {
-            const string apiKey = "sk_test_xRMH8A7bagp2Auj7YPqNihlY";
-            var accountService = new AccountService(new StripeClient(apiKey));
+        //public async Task<Account> TestAccount(string email, PersonCreateOptions individual, AccountBankAccountOptions externalAccount)
+        //{
+        //    const string apiKey = "sk_test_xRMH8A7bagp2Auj7YPqNihlY";
+        //    var accountService = new AccountService(new StripeClient(apiKey));
 
-            await accountService.UpdateAsync("", new AccountUpdateOptions
-            {
-                Individual = new PersonUpdateOptions
-                {
-                    FirstName = "Francis",
-                    LastName = "Quenneville",
-                    Dob = new DobOptions
-                    {
-                        Day = 6,
-                        Month = 5,
-                        Year = 1995
-                    },
-                    Address = new AddressOptions
-                    {
-                        Line1 = "1225 Notre-Dame W",
-                        City = "Montreal",
-                        Country = "CA",
-                        PostalCode = "H3C 6S3",
-                        State = "QC"
-                    },
-                    Email = email
-                },
-                Email = email,
-                TosAcceptance = new AccountTosAcceptanceOptions
-                {
-                    Date = DateTime.UtcNow,
-                    Ip = "10.10.10.10"
-                },
-                ExternalAccount = externalAccount,
-                Settings = new AccountSettingsOptions
-                {
-                    Payouts = new AccountSettingsPayoutsOptions
-                    {
-                        StatementDescriptor = "eDoxa Withdrawal"
-                    }
-                }
-            });
+        //    await accountService.UpdateAsync("", new AccountUpdateOptions
+        //    {
+        //        Individual = new PersonUpdateOptions
+        //        {
+        //            FirstName = "Francis",
+        //            LastName = "Quenneville",
+        //            Dob = new DobOptions
+        //            {
+        //                Day = 6,
+        //                Month = 5,
+        //                Year = 1995
+        //            },
+        //            Address = new AddressOptions
+        //            {
+        //                Line1 = "1225 Notre-Dame W",
+        //                City = "Montreal",
+        //                Country = "CA",
+        //                PostalCode = "H3C 6S3",
+        //                State = "QC"
+        //            },
+        //            Email = email
+        //        },
+        //        Email = email,
+        //        TosAcceptance = new AccountTosAcceptanceOptions
+        //        {
+        //            Date = DateTime.UtcNow,
+        //            Ip = "10.10.10.10"
+        //        },
+        //        ExternalAccount = externalAccount,
+        //        Settings = new AccountSettingsOptions
+        //        {
+        //            Payouts = new AccountSettingsPayoutsOptions
+        //            {
+        //                StatementDescriptor = "eDoxa Withdrawal"
+        //            }
+        //        }
+        //    });
 
-            return await accountService.CreateAsync(
-                new AccountCreateOptions
-                {
-                    Individual = individual,
-                    BusinessType = "individual",
-                    Type = "custom",
-                    Email = email,
-                    TosAcceptance = new AccountTosAcceptanceOptions
-                    {
-                        Date = DateTime.UtcNow,
-                        Ip = "10.10.10.10"
-                    },
-                    ExternalAccount = externalAccount,
-                    Settings = new AccountSettingsOptions
-                    {
-                        Payouts = new AccountSettingsPayoutsOptions
-                        {
-                            StatementDescriptor = "eDoxa Withdrawal"
-                        }
-                    }
-                });
-        }
+        //    return await accountService.CreateAsync(
+        //        new AccountCreateOptions
+        //        {
+        //            Individual = individual,
+        //            BusinessType = "individual",
+        //            Type = "custom",
+        //            Email = email,
+        //            TosAcceptance = new AccountTosAcceptanceOptions
+        //            {
+        //                Date = DateTime.UtcNow,
+        //                Ip = "10.10.10.10"
+        //            },
+        //            ExternalAccount = externalAccount,
+        //            Settings = new AccountSettingsOptions
+        //            {
+        //                Payouts = new AccountSettingsPayoutsOptions
+        //                {
+        //                    StatementDescriptor = "eDoxa Withdrawal"
+        //                }
+        //            }
+        //        });
+        //}
 
-        [Fact]
-        public async Task Test1()
-        {
-            var externalAccount = new AccountBankAccountOptions
-            {
-                AccountHolderName = "Francis Quenneville",
-                Country = "CA",
-                Currency = "cad",
-                AccountNumber = "000123456789",
-                RoutingNumber = "11000-000"
-            };
+        //[Fact]
+        //public async Task Test1()
+        //{
+        //    var externalAccount = new AccountBankAccountOptions
+        //    {
+        //        AccountHolderName = "Francis Quenneville",
+        //        Country = "CA",
+        //        Currency = "cad",
+        //        AccountNumber = "000123456789",
+        //        RoutingNumber = "11000-000"
+        //    };
 
-            const string email = "francis@edoxa.gg";
+        //    const string email = "francis@edoxa.gg";
 
-            var account = await this.TestAccount(
-                email,
-                new PersonCreateOptions
-                {
-                    FirstName = "Francis",
-                    LastName = "Quenneville",
-                    Dob = new DobOptions
-                    {
-                        Day = 6,
-                        Month = 5,
-                        Year = 1995
-                    },
-                    Address = new AddressOptions
-                    {
-                        Line1 = "1225 Notre-Dame W",
-                        City = "Montreal",
-                        Country = "CA",
-                        PostalCode = "H3C 6S3",
-                        State = "QC"
-                    },
-                    Email = email
-                },
-                externalAccount);
-        }
+        //    var account = await this.TestAccount(
+        //        email,
+        //        new PersonCreateOptions
+        //        {
+        //            FirstName = "Francis",
+        //            LastName = "Quenneville",
+        //            Dob = new DobOptions
+        //            {
+        //                Day = 6,
+        //                Month = 5,
+        //                Year = 1995
+        //            },
+        //            Address = new AddressOptions
+        //            {
+        //                Line1 = "1225 Notre-Dame W",
+        //                City = "Montreal",
+        //                Country = "CA",
+        //                PostalCode = "H3C 6S3",
+        //                State = "QC"
+        //            },
+        //            Email = email
+        //        },
+        //        externalAccount);
+        //}
     }
 }
