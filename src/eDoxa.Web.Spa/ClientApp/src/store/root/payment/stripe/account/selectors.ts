31694806aaa3ed735dd76bc3052165f3a93da8c0
Francis Quenneville <francis@edoxa.gg>
2019-10-22 17:52:18 -0400

    TypeScript error fixed

--- src/eDoxa.Web.Spa/ClientApp/src/store/root/payment/stripe/account/selectors.ts
+++ src/eDoxa.Web.Spa/ClientApp/src/store/root/payment/stripe/account/selectors.ts
@@ -1,4 +1,4 @@
 import * as Stripe from "stripe";
 import { AxiosError } from "axios";
 
-export const selectHasAccountVerified = (account: Stripe.accounts.IAccount, error?: AxiosError): boolean => !(!error && account && account.requirements.currently_due.length) || false;
+export const selectHasAccountVerified = (account: Stripe.accounts.IAccount, error?: AxiosError | string): boolean => !(!error && account && account.requirements.currently_due.length) || false;
