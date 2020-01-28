c09e06b3717bc2495e66b6dd843c06ef722019a8
Francis Quenneville <francis@edoxa.gg>
2020-01-27 22:05:19 -0500

    TASK 1261: Need more information in bank account
    Related Work Items: #1261

--- src/eDoxa.Web.Spa/ClientApp/src/components/Payment/Stripe/BankAccount/Panel/index.tsx
+++ src/eDoxa.Web.Spa/ClientApp/src/components/Payment/Stripe/BankAccount/Panel/index.tsx
@@ -34,35 +34,43 @@ const Panel: FunctionComponent<Props> = ({
   const disabled = buttonDisabled || !hasBankAccount;
   return (
     <Card className={`card-accent-primary ${className}`}>
-      <CardHeader className="d-flex">
-        <strong className="text-uppercase my-auto">BANK ACCOUNT</strong>
-        <Button.Link
-          className="p-0 ml-auto my-auto"
-          icon={faEdit}
-          disabled={disabled}
-          onClick={() => setButtonDisabled(true)}
-        >
-          UPDATE
-        </Button.Link>
+      <CardHeader>
+        <div className="d-block d-flex">
+          <strong className="text-uppercase my-auto">BANK ACCOUNT</strong>
+          <Button.Link
+            className="p-0 ml-auto my-auto"
+            icon={faEdit}
+            disabled={disabled}
+            onClick={() => setButtonDisabled(true)}
+          >
+            UPDATE
+          </Button.Link>
+        </div>
+        <div className="d-block mt-1 text-muted">
+          You can withdraw money from your eDoxa cashier and to deposit the
+          winnings to your bank account. Enjoy the fruits of your passion!
+        </div>
       </CardHeader>
       <CardBody>
         {loading ? (
           <Loading />
         ) : (
-          <dl className="row mb-0">
-            <dd className="col-sm-3 text-muted mb-0">Bank account</dd>
-            <dd className="col-sm-5 mb-0">
-              {disabled && (
-                <Elements>
-                  <BankAccountForm.Update
-                    country={country}
-                    handleCancel={() => setButtonDisabled(false)}
-                  />
-                </Elements>
-              )}
-              {!disabled && <span>XXXXX-{data.last4}</span>}
-            </dd>
-          </dl>
+          <>
+            <dl className="row mb-0">
+              <dd className="col-sm-3 text-muted mb-0">Bank account</dd>
+              <dd className="col-sm-5 mb-0">
+                {disabled && (
+                  <Elements>
+                    <BankAccountForm.Update
+                      country={country}
+                      handleCancel={() => setButtonDisabled(false)}
+                    />
+                  </Elements>
+                )}
+                {!disabled && <span>XXXXX-{data.last4}</span>}
+              </dd>
+            </dl>
+          </>
         )}
       </CardBody>
     </Card>
