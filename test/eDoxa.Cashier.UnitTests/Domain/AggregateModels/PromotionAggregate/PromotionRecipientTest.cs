322b1ef260b6abc665b2b56bbfd160f86b8abc74
Gabriel Roy <gabriel@edoxa.gg>
2020-02-06 22:12:55 -0500

    Promotion unit tests done

--- test/eDoxa.Cashier.UnitTests/Domain/AggregateModels/PromotionAggregate/PromotionRecipientTest.cs
+++ test/eDoxa.Cashier.UnitTests/Domain/AggregateModels/PromotionAggregate/PromotionRecipientTest.cs
@@ -1,21 +1,44 @@
 ﻿// Filename: PromotionRecipientTest.cs
-// Date Created: 2020-01-28
-// 
+// Date Created: 2020-02-04
+//
 // ================================================
 // Copyright © 2020, eDoxa. All rights reserved.
 
+using eDoxa.Cashier.Domain.AggregateModels;
+using eDoxa.Cashier.Domain.AggregateModels.PromotionAggregate;
 using eDoxa.Cashier.TestHelper;
 using eDoxa.Cashier.TestHelper.Fixtures;
+using eDoxa.Seedwork.Domain;
+using eDoxa.Seedwork.Domain.Misc;
+
+using FluentAssertions;
+
+using Xunit;
 
 namespace eDoxa.Cashier.UnitTests.Domain.AggregateModels.PromotionAggregate
 {
-    public sealed class PromotionRecipientTest : UnitTest // GABRIEL: UNIT TESTS
+    public sealed class PromotionRecipientTest : UnitTest
     {
         public PromotionRecipientTest(TestDataFixture testData, TestMapperFixture testMapper, TestValidator testValidator) : base(
             testData,
             testMapper,
             testValidator)
         {
+        }
+
+        private static User GenerateUser()
+        {
+            return new User(new UserId());
+        }
+
+        [Fact]
+        public void ToString_WithPromotionRecipient_ShouldBeUserId()
+        {
+            var user = GenerateUser();
+
+            var promotionRecipient = new PromotionRecipient(user, new UtcNowDateTimeProvider());
+
+            promotionRecipient.ToString().Should().Be(user.Id);
         }
     }
 }
--- test/eDoxa.Cashier.UnitTests/Domain/AggregateModels/PromotionAggregate/PromotionTest.cs
+++ test/eDoxa.Cashier.UnitTests/Domain/AggregateModels/PromotionAggregate/PromotionTest.cs
@@ -1,18 +1,204 @@
 ﻿// Filename: PromotionTest.cs
-// Date Created: 2020-01-28
-// 
+// Date Created: 2020-02-04
+//
 // ================================================
 // Copyright © 2020, eDoxa. All rights reserved.
 
+using System;
+
+using eDoxa.Cashier.Domain.AggregateModels;
+using eDoxa.Cashier.Domain.AggregateModels.PromotionAggregate;
 using eDoxa.Cashier.TestHelper;
 using eDoxa.Cashier.TestHelper.Fixtures;
+using eDoxa.Seedwork.Domain;
+using eDoxa.Seedwork.Domain.Misc;
+
+using FluentAssertions;
+
+using Xunit;
 
 namespace eDoxa.Cashier.UnitTests.Domain.AggregateModels.PromotionAggregate
 {
-    public sealed class PromotionTest : UnitTest // GABRIEL: UNIT TESTS
+    public sealed class PromotionTest : UnitTest
     {
         public PromotionTest(TestDataFixture testData, TestMapperFixture testMapper, TestValidator testValidator) : base(testData, testMapper, testValidator)
         {
+        }
+
+        private const string TestCode = "TestCode";
+
+        private static User GenerateUser()
+        {
+            return new User(new UserId());
+        }
+
+        [Fact]
+
+        //Francis: Comment je devrais nommer cette methode la ???
+        public void Cancel_IfCanCancel_ShouldCancel()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            var cancelTime = new UtcNowDateTimeProvider();
+
+            promotion.Cancel(cancelTime);
+
+            promotion.CanceledAt.Should().Be(cancelTime.DateTime);
+        }
+
+        [Fact]
+        public void Cancel_IfCantCancel_ShouldThrowInvalidOperationException()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(0),
+                new UtcNowDateTimeProvider());
+
+            var cancelTime = new UtcNowDateTimeProvider();
+
+            var action = new Action(() => promotion.Cancel(cancelTime));
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void IsActive_WhenActive_ShouldBeTrue()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            promotion.IsActive().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsActive_WhenNotActive_ShouldBeFalse()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(5),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            promotion.IsActive().Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsCanceled_WhenCancelled_ShouldBeTrue()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            promotion.Cancel(new UtcNowDateTimeProvider());
+            promotion.IsCanceled().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsCanceled_WhenNotCancelled_ShouldBeFalse()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            promotion.IsCanceled().Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsExpired_WhenExpired_ShouldBeTrue()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(2),
+                new DateTimeProvider(DateTime.Now));
+
+            promotion.IsExpired().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsExpired_WhenNotExpired_ShouldBeFalse()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            promotion.IsExpired().Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsRedeemBy_WhenEmpty_ShouldBeFalse()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            var promotionRecipient = new PromotionRecipient(GenerateUser(), new UtcNowDateTimeProvider());
+            promotion.IsRedeemBy(promotionRecipient).Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsRedeemBy_WhenRedeemed_ShouldBeTrue()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            var promotionRecipient = new PromotionRecipient(GenerateUser(), new UtcNowDateTimeProvider());
+
+            promotion.Redeem(promotionRecipient);
+
+            promotion.IsRedeemBy(promotionRecipient).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Redeem_WithInvalidPromotion_ShouldThrowInvalidOperationException()
+        {
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(0),
+                new UtcNowDateTimeProvider());
+
+            var promotionRecipient = new PromotionRecipient(GenerateUser(), new UtcNowDateTimeProvider());
+
+            var action = new Action(() => promotion.Redeem(promotionRecipient));
+
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void Redeem_WithValidPromotion_ShouldHaveCountOfOne()
+        {
+            //Francis: Pourquoi est-ce que il y a une duration et une date d<expiration, se serait pas mieux une date de début et une duration ????
+            //Aussi, UtcDateTimeProvider est briser, il est toujours une journée dans le futur.
+            var promotion = new Promotion(
+                TestCode,
+                new Money(50),
+                TimeSpan.FromDays(30),
+                new DateTimeProvider(DateTime.Now.AddDays(30)));
+
+            var promotionRecipient = new PromotionRecipient(GenerateUser(), new UtcNowDateTimeProvider());
+
+            promotion.Redeem(promotionRecipient);
+
+            promotion.Recipients.Should().HaveCount(1);
         }
     }
 }
