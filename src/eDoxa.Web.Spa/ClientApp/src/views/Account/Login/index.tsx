f269cd298f36759572ae6cdcde2c3db61d5c1c6b
Francis Quenneville <francis@edoxa.gg>
2020-02-09 16:00:04 -0500

    TASK 1349
    Related Work Items: #1349

--- src/eDoxa.Web.Spa/ClientApp/src/views/Account/Login/index.tsx
+++ src/eDoxa.Web.Spa/ClientApp/src/views/Account/Login/index.tsx
@@ -19,9 +19,9 @@ const Login: FunctionComponent = () => (
   <Layout.Background>
     <Container className="h-100">
       <Row className="justify-content-center">
-        <Col md="8">
+        <Col md="8 d-flex">
           <LinkContainer to={getHomePath()}>
-            <CardImg className="my-5" src={logo} />
+            <CardImg className="mt-5 mb-1 w-75 mx-auto" src={logo} />
           </LinkContainer>
         </Col>
       </Row>
@@ -29,10 +29,12 @@ const Login: FunctionComponent = () => (
         <Col md="8">
           <CardGroup className="my-5">
             <Card className="p-4">
-              <CardBody>
-                <h1>Login</h1>
-                <p className="text-muted">Sign In to your account</p>
-                <UserAccountForm.Login />
+              <CardBody className="d-flex">
+                <div className="my-auto w-100">
+                  <h1>Login</h1>
+                  <p className="text-muted">Sign In to your account</p>
+                  <UserAccountForm.Login />
+                </div>
               </CardBody>
             </Card>
             <Card
@@ -40,13 +42,13 @@ const Login: FunctionComponent = () => (
               style={{ width: "44%" }}
             >
               <CardBody className="text-center">
-                <div>
+                <div className="my-auto w-100">
                   <h2>Sign up</h2>
-                  <p>
+                  <p className="text-justify">
                     eDoxa wants to help unlock your potential so that you can be
                     proud to live your eSport dream.
                   </p>
-                  <p>
+                  <p className="text-justify">
                     Participate to Challenges for the opportunity of making
                     money and change the perception of eSport a dollar at a
                     time!
