40e18dfbdd5955131c21afd000ccbc69c567ea5d
Francis Quenneville <francis@edoxa.gg>
2020-02-09 19:13:59 -0500

    FAQ part 1

--- src/eDoxa.Web.Spa/ClientApp/src/utils/coreui/_routes.ts
+++ src/eDoxa.Web.Spa/ClientApp/src/utils/coreui/_routes.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { RouteConfig } from "../router/types";
 import {
   getHomePath,
-  getTermsOfServicesPath, 
+  getTermsOfServicesPath,
   getFaqPath,
   getNewsFeedsPath,
   getProfilePath,
@@ -28,15 +28,103 @@ const ClanDetails = React.lazy(() => import("views/Clans/Details"));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 export const routes: RouteConfig[] = [
-  { path: getHomePath(), name: "Home", component: Challenges, exact: true, allowAnonymous: true, disabled: false, scopes: [] },
-  { path: getTermsOfServicesPath(), name: "Terms of Services", component: TermsOfServices, exact: true, allowAnonymous: true, disabled: false, scopes: [] },
-  { path: getFaqPath(), name: "FAQ", component: FAQ, exact: true, allowAnonymous: true, disabled: true, scopes: [] },
-  { path: getNewsFeedsPath(), name: "News Feeds", component: NewsFeeds, exact: true, allowAnonymous: true, disabled: false, scopes: [] },
-  { path: getChallengesPath(), name: "Challenges", component: Challenges, exact: true, allowAnonymous: true, disabled: false, scopes: [] },
-  { path: getChallengeDetailsPath(), name: "Challenge Details", component: ChallengeDetails, exact: true, allowAnonymous: false, disabled: false, scopes: [] },
-  { path: getChallengeHistoryPath(), name: "Challenge History", component: ChallengeHistory, exact: true, allowAnonymous: false, disabled: false, scopes: [] },
-  { path: getClansPath(), name: "Clans", component: Clans, exact: true, allowAnonymous: false, disabled: false, scopes: [] },
-  { path: getClanDetailsPath(), name: "Clan Details", component: ClanDetails, exact: true, allowAnonymous: false, disabled: false, scopes: [] },
-  { path: getClanDashboardPath(), name: "Clan Dashboard", component: ClanDashboard, exact: true, allowAnonymous: false, disabled: false, scopes: [] },
-  { path: getProfilePath(), name: "User Profile Overview", component: Profile, exact: false, allowAnonymous: false, disabled: false, scopes: [] }
+  {
+    path: getHomePath(),
+    name: "Home",
+    component: Challenges,
+    exact: true,
+    allowAnonymous: true,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getTermsOfServicesPath(),
+    name: "Terms of Services",
+    component: TermsOfServices,
+    exact: true,
+    allowAnonymous: true,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getFaqPath(),
+    name: "FAQ",
+    component: FAQ,
+    exact: true,
+    allowAnonymous: true,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getNewsFeedsPath(),
+    name: "News Feeds",
+    component: NewsFeeds,
+    exact: true,
+    allowAnonymous: true,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getChallengesPath(),
+    name: "Challenges",
+    component: Challenges,
+    exact: true,
+    allowAnonymous: true,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getChallengeDetailsPath(),
+    name: "Challenge Details",
+    component: ChallengeDetails,
+    exact: true,
+    allowAnonymous: false,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getChallengeHistoryPath(),
+    name: "Challenge History",
+    component: ChallengeHistory,
+    exact: true,
+    allowAnonymous: false,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getClansPath(),
+    name: "Clans",
+    component: Clans,
+    exact: true,
+    allowAnonymous: false,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getClanDetailsPath(),
+    name: "Clan Details",
+    component: ClanDetails,
+    exact: true,
+    allowAnonymous: false,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getClanDashboardPath(),
+    name: "Clan Dashboard",
+    component: ClanDashboard,
+    exact: true,
+    allowAnonymous: false,
+    disabled: false,
+    scopes: []
+  },
+  {
+    path: getProfilePath(),
+    name: "User Profile Overview",
+    component: Profile,
+    exact: false,
+    allowAnonymous: false,
+    disabled: false,
+    scopes: []
+  }
 ];
