e455ed971bd08f4e0e0614599341372a99b8b94c
Gabriel Roy <gabriel@edoxa.gg>
2020-02-18 05:15:03 -0500

    domain events 100% coverage

---
+++ test/eDoxa.Cashier.UnitTests/Domain/DomainEvents/ChallengeClosedDomainEventTest.cs
@@ -0,0 +1,41 @@
+﻿// Filename: ChallengeClosedDomainEventTest.cs
+// Date Created: 2020-02-17
+//
+// ================================================
+// Copyright © 2020, eDoxa. All rights reserved.
+
+using eDoxa.Cashier.Domain.AggregateModels.ChallengeAggregate;
+using eDoxa.Cashier.Domain.DomainEvents;
+using eDoxa.Cashier.TestHelper;
+using eDoxa.Cashier.TestHelper.Fixtures;
+using eDoxa.Seedwork.Domain.Misc;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace eDoxa.Cashier.UnitTests.Domain.DomainEvents
+{
+    public sealed class ChallengeClosedDomainEventTest : UnitTest
+    {
+        public ChallengeClosedDomainEventTest(TestDataFixture testData, TestMapperFixture testMapper, TestValidator testValidator) : base(
+            testData,
+            testMapper,
+            testValidator)
+        {
+        }
+
+        [Fact]
+        public void ChallengeClosedDomainEvent_ShouldBeValid()
+        {
+            // Arrange
+            var challengeId = new ChallengeId();
+            var payouts = new ChallengeParticipantPayouts();
+            var domainEvent = new ChallengeClosedDomainEvent(challengeId, payouts);
+
+            // Assert
+            domainEvent.ChallengeId.Should().Be(challengeId);
+            domainEvent.Payouts.Should().BeEquivalentTo(payouts);
+        }
+    }
+}
