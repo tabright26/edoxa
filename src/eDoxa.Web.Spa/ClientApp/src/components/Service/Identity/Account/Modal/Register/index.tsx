cae467bf4f4e475b4be87bc349ee9498ef61ce67
Francis Quenneville <francis@edoxa.gg>
2020-02-21 15:09:49 -0500

    BUG 1431
    Related Work Items: #1431

--- src/eDoxa.Web.Spa/ClientApp/src/components/Service/Identity/Account/Modal/Register/index.tsx
+++ src/eDoxa.Web.Spa/ClientApp/src/components/Service/Identity/Account/Modal/Register/index.tsx
@@ -15,7 +15,19 @@ type Props = InnerProps & OutterProps;
 const Register: FunctionComponent<Props> = ({ show, handleHide }) => (
   <Modal backdrop="static" centered isOpen={show} toggle={handleHide}>
     <ModalHeader className="bg-gray-900" toggle={handleHide}>
-      <strong className="text-uppercase">REGISTER</strong>
+      <h4 className="text-light">
+        <strong className="text-uppercase">
+          REGISTER{" "}
+          <span className="text-primary" style={{ fontSize: "125%" }}>
+            NOW
+          </span>{" "}
+          AND GET{" "}
+          <span className="text-primary" style={{ fontSize: "125%" }}>
+            $5
+          </span>{" "}
+          USD!
+        </strong>
+      </h4>
       <ModalSubtitle>Create your account</ModalSubtitle>
     </ModalHeader>
     <ModalBody>{show && <AccountForm.Register />}</ModalBody>
