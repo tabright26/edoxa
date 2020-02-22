de04b356cf5bdabd8f0a08f06cdacfd66657e787
Francis Quenneville <francis@edoxa.gg>
2020-02-22 16:13:28 -0500

    Global options classes

---
+++ src/eDoxa.Seedwork.Application/Options/ClientWebEndpointsOptions.cs
@@ -0,0 +1,15 @@
+﻿// Filename: ClientWebEndpointsOptions.cs
+// Date Created: 2020-02-22
+// 
+// ================================================
+// Copyright © 2020, eDoxa. All rights reserved.
+
+#nullable disable
+
+namespace eDoxa.Seedwork.Application.Options
+{
+    public sealed class ClientWebEndpointsOptions
+    {
+        public string WebSpaUrl { get; set; }
+    }
+}
---
+++ src/eDoxa.Seedwork.Application/Options/GatewayWebEndpointsOptions.cs
@@ -0,0 +1,17 @@
+﻿// Filename: GatewayWebEndpointsOptions.cs
+// Date Created: 2020-02-22
+// 
+// ================================================
+// Copyright © 2020, eDoxa. All rights reserved.
+
+#nullable disable
+
+namespace eDoxa.Seedwork.Application.Options
+{
+    public sealed class GatewayWebEndpointsOptions
+    {
+        public string CashierUrl { get; set; }
+
+        public string ChallengesUrl { get; set; }
+    }
+}
